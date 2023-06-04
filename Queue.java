474751403370b4fe458c211bfab34c396f0b877c
Anette-Gallegos <agallegosco@unsa.edu.pe>
2023-06-04 08:44:50 -0500

    Queue: Agregando el constructor y la función add

--- Queue.java
+++ Queue.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Queue<E> {
     private List<E> queue;
+
+    //Constructor
+    public Queue() {
+        queue = new ArrayList<>();
+    }
+
+    //Función agregar
+    public boolean add(E e) {
+        return queue.add(e);
+    }
 }
